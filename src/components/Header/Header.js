d617bf6c6344b10d55951170e54e60c351c3479d
Boni-amin <boniamin.info@gmail.com>
2021-09-26 17:12:14 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="text-center text-primary py-2">
+            <h1>Make an Online School</h1>
+            <h2 className="h1">Total Budget $50 Million</h2>
+            {/* <input class="btn btn-primary" type="button" value="Input"></input> */}
+        </div>
+    );
+};
+
+export default Header;
